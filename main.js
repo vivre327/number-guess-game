aa1d8a0f37cf73eddf6c8a031520a203ce15a5d6
vivre327 <hhaayy27@naver.com>
2025-09-08 22:30:51 +0900

    add. RESET기능과 유효성검사 로직 추가

--- main.js
+++ main.js
@@ -12,8 +12,17 @@ let computerNum = 0;
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chances = 5;
+let gameOver = false;
+let chanceArea = document.getElementById("chance-area");
+let history = [];
 
 playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
+userInput.addEventListener("focus",()=>{
+  userInput.value = "";
+})
 
 function pickRandomNum() {
   computerNum = Math.floor(Math.random() * 100) + 1;
@@ -22,7 +31,24 @@ function pickRandomNum() {
 
 function play(){
   let userValue = userInput.value;
-  console.log(userValue);
+  // console.log(userValue);
+  
+  console.log("chance",chances);
+  
+  // 유효성검사 : 올바른 값인지
+  if(userValue < 1 || userValue > 100){
+    chanceArea.textContent = "1과 100사이 숫자를 입력해 주세요"
+    return; //종료
+  }
+
+  // 유효성검사 : 중복된 값인지
+  if(history.includes(userValue)){
+    resultArea.textContent = "이미 입력한 숫자입니다. 다른 숫자를 입력해 주세요"
+    return; //종료
+  }
+
+  chances --; // 클릭시 1씩 감소
+  chanceArea.textContent = `남은기회: ${chances}번`
 
   if(userValue < computerNum){
     resultArea.textContent = "Up!!";
@@ -30,7 +56,26 @@ function play(){
     resultArea.textContent = "Down!!!";
   } else {
     resultArea.textContent = "맞추셨습니다!!!";
+    gameOver = true;
   }
+
+  history.push(userValue);
+  console.log(history);
+
+  if(chances <1){
+    gameOver = true;
+  }
+  if (gameOver == true) {
+    playButton.disabled = true;
+  }
+}
+
+function reset(){
+  // console.log("리셋!");
+  userInput.value = "";
+  pickRandomNum();
+  resultArea.textContent = "결과값이 여기 나옵니다!";
+  playButton.disabled = false;
 }
 
 pickRandomNum();
